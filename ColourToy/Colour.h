6519cb041b61724a7745b9e6a4d30df57f3e44d4
Chris Whitworth <chris.whitworth@red-gate.com>
2014-06-23 12:31:04 +0100

    Refine colour macros, add Distance function, plus some "tests".

--- ColourToy/Colour.h
+++ ColourToy/Colour.h
@@ -1,23 +1,33 @@
 #pragma once
 
-#define R(col) ( (col) & 0xff )
-#define G(col) ( ( (col) >> 8 ) & 0xff )
-#define B(col) ( ( (col) >> 16 ) & 0xff )
-#define A(col) ( ( (col) >> 24 ) & 0xff )
+#define R(col) ((unsigned char)( (col) & 0xff ))
+#define G(col) ((unsigned char)( ( (col) >> 8 ) & 0xff ))
+#define B(col) ((unsigned char)( ( (col) >> 16 ) & 0xff ))
+#define A(col) ((unsigned char)( ( (col) >> 24 ) & 0xff ))
 
-#define Colour(r,g,b,a) \
-    ( \
+#define ColourA(r,g,b,a) \
+    ((unsigned)( \
         ( (r) & 0xff ) |\
         ( ( (g) & 0xff ) << 8) |\
         ( ( (b) & 0xff ) << 16) |\
         ( ( (a) & 0xff ) << 24)\
-    )
+    ))
 
 #define Colour(r,g,b) \
-    ( \
+    ((unsigned)( \
         ( (r) & 0xff ) |\
         ( ( (g) & 0xff ) << 8) |\
         ( ( (b) & 0xff ) << 16) |\
         ( 0xff << 24)\
-    )
+    ))
 
+inline unsigned Distance(unsigned c1, unsigned c2)
+{
+    int r = R(c1) - R(c2);
+    int g = G(c1) - G(c2);
+    int b = B(c1) - B(c2);
+
+    auto d = ((r*r) + (g*g) + (b*b));
+
+    return (unsigned) d;
+}
--- ColourToy/ColourToy.cpp
+++ ColourToy/ColourToy.cpp
@@ -27,6 +27,7 @@ int _tmain(int argc, _TCHAR* argv[])
     image.Plot(10, 10, Colour(0xff, 0, 0xff));
 
 
+
     auto err = lodepng::encode(OUTPUT, image.RawData(), WIDTH, HEIGHT);
 
     return 0;
@@ -35,4 +36,12 @@ int _tmain(int argc, _TCHAR* argv[])
 void Sanity()
 {
     assert(Colour(0xff, 0, 0) == 0xff0000ff);
+    
+    auto c1 = Colour(0xff, 0x00, 0x00);
+    auto c2 = Colour(0xfe, 0x00, 0x00);
+    auto c3 = Colour(0xff, 0x01, 0x00);
+
+    assert(Distance(c1, c2) == Distance(c1, c3));
+
+    assert(Distance(c1, c2) < Distance(c2, c3));
 }
